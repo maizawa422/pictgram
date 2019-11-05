6792a5a374a92b31f230ea4993774bc2c023b567
Misaki <maizawa.apr22@gmail.com>
2019-11-05 15:02:49 +0000

    rails06

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,19 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+  def new
+    @topic = Topic.new
+  end
+  
+  def create
+    @topic = current_user.topics.new(topic_params)
+    
+    if @topic.save
+      redirect_to topics_path, success: '投稿に成功しました'
+    else
+      flash.now[:danger] = '投稿に失敗しました'
+      render :new
+    end
+  end
+end
